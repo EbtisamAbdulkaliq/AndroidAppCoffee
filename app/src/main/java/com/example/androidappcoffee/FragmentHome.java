a2da557a048f6d8cca01b388de5a172ce345a3a4
unknown <eabdulk1@centennialcollege.ca>
2023-12-07 14:57:57 -0500

    RelativeLayout CREATED

---
+++ app/src/main/java/com/example/androidappcoffee/FragmentHome.java
@@ -0,0 +1,4 @@
+package com.example.androidappcoffee;
+
+public class FragmentHome {
+}
---
+++ app/src/main/java/com/example/androidappcoffee/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.androidappcoffee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+    fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container, fragment)
+            addToBackStack(null)
+            commit()
+        }
+    }
+}
