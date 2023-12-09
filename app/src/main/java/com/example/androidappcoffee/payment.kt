56f40a7a2840f2e4ac661752b4421f9e20f39f3e
unknown <eabdulk1@centennialcollege.ca>
2023-12-09 03:04:32 -0500

    initilized the views and editText

--- app/src/main/java/com/example/androidappcoffee/payment.kt
+++ app/src/main/java/com/example/androidappcoffee/payment.kt
@@ -5,20 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
 
 
 class payment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var editTextFullName: EditText
+    private lateinit var editTextPhoneNumber: EditText
+    private lateinit var editTextEmail: EditText
+    private lateinit var editTextAddress: EditText
+    private lateinit var editTextCity: EditText
+    private lateinit var editTextZipCode: EditText
+    private lateinit var editTextCountry: EditText
+    private lateinit var editTextCardHolderName: EditText
+    private lateinit var editTextCardNumber: EditText
+    private lateinit var editTextExpiryDate: EditText
+    private lateinit var editTextCVV: EditText
+    private lateinit var timePicker: TimePicker
+    private lateinit var buttonPlaceOrder: Button
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,23 +34,40 @@ class payment : Fragment() {
         return inflater.inflate(R.layout.fragment_payment, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment payment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            payment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize views
+        editTextFullName = view.findViewById(R.id.editTextFullName)
+        editTextPhoneNumber = view.findViewById(R.id.editTextPhoneNumber)
+        editTextEmail = view.findViewById(R.id.editTextEmail)
+        editTextAddress = view.findViewById(R.id.editTextAddress)
+        editTextCity = view.findViewById(R.id.editTextCity)
+        editTextZipCode = view.findViewById(R.id.editTextZipCode)
+        editTextCountry = view.findViewById(R.id.editTextCountry)
+        editTextCardHolderName = view.findViewById(R.id.editTextCardHolderName)
+        editTextCardNumber = view.findViewById(R.id.editTextCardNumber)
+        editTextExpiryDate = view.findViewById(R.id.editTextExpiryDate)
+        editTextCVV = view.findViewById(R.id.editTextCVV)
+        timePicker = view.findViewById(R.id.timePicker)
+        buttonPlaceOrder = view.findViewById(R.id.buttonPlaceOrder)
+
+        // Add text watchers for validation
+        val textWatcher = createTextWatcher()
+        editTextFullName.addTextChangedListener(textWatcher)
+        editTextPhoneNumber.addTextChangedListener(textWatcher)
+        editTextEmail.addTextChangedListener(textWatcher)
+        editTextAddress.addTextChangedListener(textWatcher)
+        editTextCity.addTextChangedListener(textWatcher)
+        editTextZipCode.addTextChangedListener(textWatcher)
+        editTextCountry.addTextChangedListener(textWatcher)
+        editTextCardHolderName.addTextChangedListener(textWatcher)
+        editTextCardNumber.addTextChangedListener(textWatcher)
+        editTextExpiryDate.addTextChangedListener(textWatcher)
+        editTextCVV.addTextChangedListener(textWatcher)
+
+
+
+
+        }
     }
-}
